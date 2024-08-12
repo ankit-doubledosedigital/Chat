875f60ea873247b2f8f614d4507d8027b4c26f16
Ankit1141 <jadounankit17@gmail.com>
2024-08-12 13:34:00 +0530

    few change

--- server/index.js
+++ server/index.js
@@ -5,7 +5,6 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const connectDB = require('./db/dbConnection');
-const authRoutes = require('./Routes/user');
 
 // Rest of your code
 
@@ -17,10 +16,7 @@ app.use(express.json());
 // Enable Cors 
 app.use(cors())
 
-app.use('/auth',require('./Routes/user'));
-// app.use('verify-otp',require('./Routes/user'));
 
-// app.use('/auth', authRoutes);
 
 
 
