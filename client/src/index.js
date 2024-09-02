bd513a149dd0b51a6d5be8d20c3d9158916bd6d9
Ankit1141 <jadounankit17@gmail.com>
2024-09-02 16:50:27 +0530

    few change

--- client/src/index.js
+++ client/src/index.js
@@ -6,15 +6,11 @@ import { GoogleOAuthProvider } from '@react-oauth/google';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
-
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <GoogleOAuthProvider clientId="793075133255-tk4vjvbupijb0cto3nmqgm8khkm7g8ib.apps.googleusercontent.com">
-    <App />
+    <GoogleOAuthProvider clientId={process.env.REACT_APP_CLIENT_ID}>
+      <App />
     </GoogleOAuthProvider>
   </React.StrictMode>
 );
-
-
